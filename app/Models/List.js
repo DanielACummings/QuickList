a83a7d791b17c6829bc2b7124609e8af0ae8b726
Daniel <danielac@protonmail.com>
2019-11-22 11:54:03 -0700

    adds list adding form

--- app/Models/List.js
+++ app/Models/List.js
@@ -1,10 +1,24 @@
 import { generateId } from "../utils.js";
 
 export default class List {
-  constructor(data) {
+  constructor({ id = generateId(), name, items }) {
     //TODO Your constructor takes in a data object that should have the properties you need to create your list here is a freebie, it will set the id its provided, or if that is undefined it will create a new one (this is an alternative to object destructuring)
-    this.id = data.id || generateId();
+    this.id = id;
+    this.name = name;
+    this.items = items
+    console.log('listmodel');
   }
   //Be sure to add the methods needed to create the view template for this model
   //For starting out, your tasks may be strings alone, but later you may wish to turn them into full objects, that will be up to you
+  get template() {
+    return /*html*/ `
+      
+    `
+  }
+
+  // drawItems() {
+  //   let template = ''
+  //   this.items.forEach(items => template += items.template);
+  //   return template
+  // }
 }
