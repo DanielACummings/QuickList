19d55e4f324dc9376a92cd349699d8fdd9e2db58
Daniel <danielac@protonmail.com>
2019-11-25 11:36:59 -0700

    adds feature: items can be deleted

--- app/Services/ListService.js
+++ app/Services/ListService.js
@@ -6,7 +6,6 @@ class ListService {
   addItem(newItem) { //why is listId never called? It's used below
     let item = new Item(newItem)
     let foundList = _store.State.lists.find(lists => lists.id == item.listId)
-    debugger
     foundList.items.push(item) //push(item.newItem) ?
     _store.saveState()
     console.log('additemservice');
@@ -14,7 +13,7 @@ class ListService {
   delItem(listId, itemId) { //listID never gets defined in item model constructor
     let close = confirm("Are you sure you\nwant to delete this?")
     if (close == true) {
-      //listToRemItemsFrom is undefined according to debugger in Chrome
+      //listToRemItemsFrom is undefined 
       let listToRemItemFrom = _store.State.lists.find(list => list.id == listId) //use find() or findIndex()? After switching to find(), listToRemItemFrom is no longer 'number' type--that got rid of an intellisense error.
       // use []?
       let itemToRem = listToRemItemFrom.items.findIndex(item => item.id == itemId)
